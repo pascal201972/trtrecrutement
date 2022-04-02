5937d124a91d1bd7ac6eb41cc25b3e1d11e96b10
pascal jasmin <jasminpascal2016@gmail.com>
2022-04-02 19:40:27 +0200

    installation de l'espace membre, page de connection, des identifiant de l'administarteur et de la pase de donnée

---
+++ src/DataFixtures/Userfixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\TrtUser;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class Userfixtures extends Fixture
+{
+    private $passwordEncoder;
+    public function __construct(UserPasswordHasherInterface $passwordEncoder_)
+    {
+        $this->passwordEncoder = $passwordEncoder_;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        $user1 = new TrtUser();
+        $user1->setEmail('jasminpascal2016@gmail.com');
+        $user1->setRoles(['ROLE_ADMIN']);
+
+        $user1->setPassword($this->passwordEncoder->hashPassword($user1, 'AdminJasmin!2022'));
+        $manager->persist($user1);
+
+        $manager->flush();
+    }
+}
